5c52b117b46de481b2b09a5a347c86c3d92dbe3b
tuanna <tuan.na2214@gmail.com>
2021-08-28 21:13:40 +0700

    Initial commit

---
+++ src/main/java/com/project/FFTCnow/FftCnowApplication.java
@@ -0,0 +1,13 @@
+package com.project.FFTCnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FftCnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FftCnowApplication.class, args);
+	}
+
+}
